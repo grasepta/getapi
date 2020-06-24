de711d7dedd42d599ee1590f86ed7112cb35307f
Aldino <grasepta@gmail.com>
2020-06-24 09:41:41 +0700

    Initial Commit

---
+++ APICheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APICheck
+//
+//  Created by Aldino Grasepta on 24/06/20.
+//  Copyright © 2020 Grasepta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
